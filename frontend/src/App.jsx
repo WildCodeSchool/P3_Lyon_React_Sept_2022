6d0cf6e6ba64ef1a3eb75a6ea69fa127baed9ced
Javier Lopez <jvrlpzd@gmail.com>
2022-12-20 09:46:45 +0100

    added carrousel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Connexion from "./pages/Connexion";
 import "./App.css";
+import Carrousel from "./components/Carrousel/Carrousel";
 
 function App() {
   return (
     <div className="App">
       <Connexion />
+      <Carrousel />
     </div>
   );
 }
--- frontend/src/components/Carrousel/Carrousel.jsx
+++ frontend/src/components/Carrousel/Carrousel.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+// eslint-disable-next-line import/no-unresolved
+import "swiper/css";
+// eslint-disable-next-line import/no-unresolved
+import "swiper/css/pagination";
 
 function Carrousel() {
-  return <div>Carrousel</div>;
+  return (
+    <div className="carrousel-container">
+      <Swiper
+        className="h-44"
+        // install Swiper modules
+        modules={[Pagination, A11y]}
+        spaceBetween={30}
+        slidesPerView={2.5}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.warn(swiper)}
+        onSlideChange={() => console.warn("slide change")}
+      >
+        <SwiperSlide className="flex bg-blue-400 justify-center items-center">
+          Slide 1
+        </SwiperSlide>
+        <SwiperSlide className="flex bg-blue-400 justify-center items-center">
+          Slide 2
+        </SwiperSlide>
+        <SwiperSlide className="flex bg-blue-400 justify-center items-center">
+          Slide 3
+        </SwiperSlide>
+        <SwiperSlide className="flex bg-blue-400 justify-center items-center">
+          Slide 4
+        </SwiperSlide>
+        <SwiperSlide className="flex bg-blue-400 justify-center items-center">
+          Slide 5
+        </SwiperSlide>
+        ...
+      </Swiper>
+    </div>
+  );
 }
 
 export default Carrousel;
