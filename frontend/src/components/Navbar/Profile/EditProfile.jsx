22681a042e4949aec8217d4a8c1043e5ed5fa31b
Doroteya Donova <doroteia.donova@gmail.com>
2023-01-02 22:05:54 +0100

    Edit my profile finished

--- frontend/src/components/Navbar/Profile/EditProfile.jsx
+++ frontend/src/components/Navbar/Profile/EditProfile.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function EditProfile() {
   return (
-    <div className="h-screen w-screen	overflow-x-hidden">
+    <div className="h-screen w-screen	overflow-x-hidden bg-white">
       <button type="button">
         <img className="ml-2 mt-6" src="./src/assets/croix.png" alt="Close" />
       </button>
@@ -12,7 +12,7 @@ function EditProfile() {
       </h1>
       <div className="flex justify-center">
         <img
-          className="rounded-full w-36 mr-2 border-4 border-violet mr-4"
+          className="rounded-full w-36 border-4 border-violet mr-4"
           src="./src/assets/my-avatar.jpeg"
           alt="My profile avatar"
         />
@@ -57,10 +57,24 @@ function EditProfile() {
           />
         </label>
       </form>
-      <h2 className="text-lg text-center mt-2 font-light">
+      <h2 className="text-lg text-center mt-2 font-light mb-4">
         Sélectionnez votre équipe
       </h2>
-      <div className="text-center mt-6">
+      <div className="flex justify-center items-center">
+        <select
+          id="equipe"
+          className="w-80 bg-gray-50 border border-primary text-gray-900 text-md rounded-lg p-3"
+        >
+          <option selected>Equipe</option>
+          <option value="Communication">Communication</option>
+          <option value="Informatique">Informatique</option>
+          <option value="RH">RH</option>
+          <option value="Developpement">Développement</option>
+          <option value="Technique">Technique</option>
+          <option value="RC">Relation client</option>
+        </select>
+      </div>
+      <div className="text-center mt-8">
         <button
           type="button"
           className="bg-primary text-white py-3 px-[2.5rem] rounded-[20px] w-6/12 text-md mb-4"
