1e8c0e820e6ca4a987e0270fc33b5e98c6545676
Doroteya Donova <doroteia.donova@gmail.com>
2023-01-04 17:34:34 +0100

    Finished modal close and open

--- frontend/src/components/Navbar/Profile/ProfileCard.jsx
+++ frontend/src/components/Navbar/Profile/ProfileCard.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../Avatar";
+import EditProfile from "./EditProfile";
 
 function ProfileCard() {
+  const [editProfile, setEditProfile] = useState(false);
+
+  function openEditProfile() {
+    setEditProfile(!editProfile);
+  }
+
   return (
     <div className="flex flex-col justify-center">
       <div className="user-avatar mx-auto rounded-full w-60 h-60 border-4 border-violet z-10">
@@ -12,6 +19,21 @@ function ProfileCard() {
           <h2 className="text-2xl font-black mt-24">Ryan Bidau</h2>
           <h3 className="text-xl italic">Project Manager</h3>
           <h3 className="text-lg mt-10 px-10">Equipe: Commercial</h3>
+          <button
+            onClick={() => openEditProfile()}
+            className="border border-primary text-primary mt-2 py-2 px-[2.5rem] rounded-[20px]"
+            type="button"
+          >
+            Edit Profile
+          </button>
+          {editProfile ? (
+            <EditProfile
+              editProfile={editProfile}
+              setEditProfile={setEditProfile}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
