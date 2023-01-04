6bbf343e12759186ef98c0702136f546b73a9195
ryanrasal <ryan.beaujot@outlook.fr>
2023-01-04 17:26:57 +0100

    update modall Create Post

--- frontend/src/components/Feed/CreatePostContainer/CreatePostContainer.jsx
+++ frontend/src/components/Feed/CreatePostContainer/CreatePostContainer.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+/* eslint-disable react/button-has-type */
+import React, { useState } from "react";
+import CreatePost from "./CreatePost";
 
 function CreatePostContainer() {
+  const [showCreatePost, setShowCreatePost] = useState(false);
+
+  function openModalFull() {
+    setShowCreatePost(!showCreatePost);
+  }
+
   return (
     <div>
       <div className="bg-white w-full shadow-md rounded-t-sm	border-t border-gray-100 mt-10 flex items-center justify-center p-6">
@@ -11,9 +18,17 @@ function CreatePostContainer() {
           alt="My profile avatar"
         />
         <div className="border border-primary w-5/6 rounded-xl h-10 pt-2">
-          <span className="ml-2 text-gray-400">
-            <Link to="/createPost">Votre publication...</Link>
-          </span>
+          <button onClick={() => openModalFull()}>
+            <span className="ml-2 text-gray-400">Votre publication...</span>
+          </button>
+          {showCreatePost ? (
+            <CreatePost
+              showCreatePost={showCreatePost}
+              setShowCreatePost={setShowCreatePost}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
