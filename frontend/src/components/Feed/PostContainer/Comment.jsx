10cb2a2b80b399c3cbacc2d757a5c85856e71e30
Doroteya Donova <doroteia.donova@gmail.com>
2023-01-18 21:27:47 +0100

    post fetch comment

--- frontend/src/components/Feed/PostContainer/Comment.jsx
+++ frontend/src/components/Feed/PostContainer/Comment.jsx
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCurrentUserContext } from "../../../contexts/userContext";
 
 function Comment() {
-  const { user } = useCurrentUserContext();
+  const { user, post } = useCurrentUserContext();
+
+  const [createComment, setCreateComment] = useState({
+    content: "",
+    user_id: "",
+    post_id: "",
+  });
+
+  const onChange = (e) => {
+    setCreateComment({
+      ...createComment,
+      content: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const body = JSON.stringify(createComment);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+    };
+    if (
+      createComment.content &&
+      createComment.user_id &&
+      createComment.post_id
+    ) {
+      // On appelle le back. Si tous les middleware placé sur la route ci-dessous, je pourrais être renvoyé à la route login
+      fetch(
+        `http://localhost:5000/api/posts/${post.id}/comments`,
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then((response) => {
+          setCreateComment(response);
+          console.warn(response);
+        })
+        .catch(console.error());
+    }
+  };
   return (
-    <div className="w-full mt-6 flex items-center justify-between pb-6">
+    <div className="w-full mt-6 ml-6 flex items-center justify-start pb-6">
       <img
         className="rounded-full w-10 h-10 mr-3 border-4 border-violet"
         src={user.avatar}
         alt="My profile avatar"
       />
-      <input
-        className="w-11/12 shadow-md rounded-xl py-2 pl-2 text-sm"
-        type="text"
-        valeur=""
-        placeholder="Laissez un commentaire..."
-      />
+      <form onSubmit={(e) => onSubmit(e)} method="PUT">
+        <input
+          className="w-72 shadow-md rounded-xl py-2 pl-2 text-sm"
+          type="text"
+          name="comment"
+          placeholder="Laissez un commentaire..."
+          valeur={createComment.content}
+          onChange={onChange}
+        />
+      </form>
     </div>
   );
 }
