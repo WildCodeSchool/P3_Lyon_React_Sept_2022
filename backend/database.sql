07b8685a6eace0935c05fdb92c26dc8eda17e195
Doroteya Donova <doroteia.donova@gmail.com>
2023-01-25 15:13:19 +0100

    Delete post refresh ok

--- backend/database.sql
+++ backend/database.sql
@@ -38,7 +38,7 @@ VALUES
     '0628164512',
     'user',
     '$argon2id$v=19$m=65536,t=5,p=1$dKODL61qs2D/2A+iFhTc9w$xJWm4TCgdKNDQdy/2KWF1AQrcKqvrljRO9Ex0byxhps',
-    'https://i.imgur.com/FHz3NQx.jpg'
+    'https://i.imgur.com/vJKyNZ2.jpg'
   ),
   (
     'Matthieu',
@@ -538,7 +538,7 @@ content text not null,
 post_id int not null,
 user_id int not null,
 CONSTRAINT fk_comment_post
-FOREIGN KEY(post_id) REFERENCES post(id),
+FOREIGN KEY(post_id) REFERENCES post(id) ON DELETE CASCADE,
 CONSTRAINT fk_comment_user
 FOREIGN KEY(user_id) REFERENCES user_detail(id)
 );
--- frontend/src/components/Feed/CreatePostContainer/CreatePostContainer.jsx
+++ frontend/src/components/Feed/CreatePostContainer/CreatePostContainer.jsx
@@ -5,8 +5,8 @@ import Avatar from "../../Navbar/Avatar";
 function CreatePostContainer() {
   return (
     <div className=" md:ml-[-85%] md:w-[66vw] md:mb-4 ">
-      <div className="bg-white w-full shadow-md rounded-t-sm	border-t border-gray-100 mt-10 flex items-center justify-center p-6 md:h-28 md:rounded-lg ">
-        <div className="rounded-full">
+      <div className="bg-white w-full shadow-md rounded-t-sm border-t border-gray-100 mt-10 flex items-center justify-center p-6 md:h-28 md:rounded-lg ">
+        <div className="rounded-full mr-3">
           <Avatar />
         </div>
 
