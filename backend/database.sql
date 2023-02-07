1a565338c06ebfe5d4aa75b8d0b756882b291839
Javier Lopez <jvrlpzd@gmail.com>
2023-02-07 16:48:17 +0100

    Fixed some minor errors with fetch and protected routes

--- backend/database.sql
+++ backend/database.sql
@@ -71,7 +71,7 @@ VALUES
     'ryan.jpg'
   );
 
-UPDATE user_detail SET is_admin = true WHERE id = 3;
+UPDATE user_detail SET is_admin = true WHERE id = 5;
 
 CREATE TABLE group_detail(
 id serial primary key not null,
@@ -613,5 +613,5 @@ CREATE TABLE user_group
 
 INSERT INTO
   user_group (user_id, group_id)
-VALUES (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (2, 5), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (4, 1), (4, 2), (4, 5), (5, 1), (5, 2), (5, 3);
+VALUES (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (2, 5), (3, 1), (3, 2), (3, 3), (4, 1), (4, 2), (4, 5), (5, 1), (5, 2), (5, 3);
 
