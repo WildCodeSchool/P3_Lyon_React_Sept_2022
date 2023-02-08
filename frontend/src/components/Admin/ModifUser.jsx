26f93078fa1ff4477d85a40f6aa866eb00bd6184
Javier Lopez <jvrlpzd@gmail.com>
2023-02-08 12:16:56 +0100

    added deletion confirmation to deleteGroup

--- frontend/src/components/Admin/ModifUser.jsx
+++ frontend/src/components/Admin/ModifUser.jsx
@@ -80,68 +80,71 @@ export default function ModifUser() {
   };
 
   return (
-    <div className="h-screen">
-      <Link to="/adminuser">
-        <button type="button">
-          <img className="mr-80 mt-3 ml-4" src={croix} alt="Close btn" />
-        </button>
-      </Link>
-      <div className="md:flex md:items-center md:justify-center">
-        <div className="font-[Enedis] text-primary text-center font-bold text-3xl md:text-4xl mb-6 md:w-48">
-          Modifier un utilisateur
+    userData.firstname && (
+      <div className="h-screen">
+        <Link to="/adminuser">
+          <button type="button">
+            <img className="mr-80 mt-3 ml-4" src={croix} alt="Close btn" />
+          </button>
+        </Link>
+        <div className="md:flex md:items-center md:justify-center">
+          <div className="font-[Enedis] text-primary text-center font-bold text-3xl md:text-4xl mb-6 md:w-48">
+            Modifier un utilisateur
+          </div>
+          <div className="flex justify-around ">
+            <img
+              className="rounded-full h-40 w-40 ml-5 border-4 border-violet"
+              src={
+                userData.avatar
+                  ? `${backEnd}/uploads/${userData.avatar}`
+                  : avatar
+              }
+              alt="User avatar"
+            />
+          </div>
         </div>
-        <div className="flex justify-around ">
-          <img
-            className="rounded-full h-40 w-40 ml-5 border-4 border-violet"
-            src={
-              userData.avatar ? `${backEnd}/uploads/${userData.avatar}` : avatar
-            }
-            alt="User avatar"
-          />
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <form
-          onSubmit={onSubmit}
-          className="flex text-base font-bold flex-col mt-1 text-black py-3"
-        >
-          Prénom :
-          <input
-            className=" border pl-2 my-3  h-12 rounded w-[80vw] border-primary"
-            type="text"
-            name="firstname"
-            placeholder="Prénom"
-            onChange={onChange}
-            value={userData.firstname}
-          />
-          Nom :
-          <input
-            className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
-            type="text"
-            name="lastname"
-            placeholder="Nom"
-            onChange={onChange}
-            value={userData.lastname}
-          />
-          Téléphone :
-          <input
-            className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
-            type="text"
-            name="phone"
-            placeholder="Téléphone"
-            onChange={onChangeNum}
-            value={userData.phone_number}
-          />
-          Email :
-          <input
-            className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
-            type="email"
-            name="email"
-            placeholder="email"
-            onChange={onChange}
-            value={userData.email}
-          />
-          {/* Mot de passe :
+        <div className="flex justify-center">
+          <form
+            onSubmit={onSubmit}
+            className="flex text-base font-bold flex-col mt-1 text-black py-3"
+          >
+            Prénom :
+            <input
+              className=" border pl-2 my-3  h-12 rounded w-[80vw] border-primary"
+              type="text"
+              name="firstname"
+              placeholder="Prénom"
+              onChange={onChange}
+              value={userData.firstname}
+            />
+            Nom :
+            <input
+              className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
+              type="text"
+              name="lastname"
+              placeholder="Nom"
+              onChange={onChange}
+              value={userData.lastname}
+            />
+            Téléphone :
+            <input
+              className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
+              type="text"
+              name="phone"
+              placeholder="Téléphone"
+              onChange={onChangeNum}
+              value={userData.phone_number}
+            />
+            Email :
+            <input
+              className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
+              type="email"
+              name="email"
+              placeholder="email"
+              onChange={onChange}
+              value={userData.email}
+            />
+            {/* Mot de passe :
           <input
             className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
             type="password"
@@ -150,24 +153,25 @@ export default function ModifUser() {
             onChange={onChangepass}
             value={userData.password}
           /> */}
-          Poste :
-          <input
-            className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
-            type="text"
-            name="role"
-            placeholder="Poste"
-            onChange={onChange}
-            value={userData.role}
-          />
-          <button
-            type="submit"
-            className="text-white h-15 w-44 p-2 mx-auto mt-4 font-[Enedis] bg-primary text-base border border-primary rounded-2xl "
-          >
-            Enregistrer
-          </button>
-        </form>
+            Poste :
+            <input
+              className=" border pl-2 h-12 my-3 rounded w-[80vw] border-primary"
+              type="text"
+              name="role"
+              placeholder="Poste"
+              onChange={onChange}
+              value={userData.role}
+            />
+            <button
+              type="submit"
+              className="text-white h-15 w-44 p-2 mx-auto mt-4 font-[Enedis] bg-primary text-base border border-primary rounded-2xl "
+            >
+              Enregistrer
+            </button>
+          </form>
+        </div>
+        <div className="flex mb-3  justify-center" />
       </div>
-      <div className="flex mb-3  justify-center" />
-    </div>
+    )
   );
 }
